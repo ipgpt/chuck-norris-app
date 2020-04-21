3ac5ac8586d651d53e736aedc2093d9782d09566
Petro Fedorenko <petewaitforletters@gmail.com>
2020-04-21 18:25:52 +0300

    feat: install bootstrap and add layout

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import { Row, Col } from 'react-bootstrap';
 import './App.scss';
 
 function App() {
   return (
     <div className="App">
-      Chuck Norris App
+      <h1>Chuck Norris App</h1>
+      
+        <Row>
+          <Col xl={9}>1 of 2</Col>
+          <Col xl={3}>2 of 2</Col>
+        </Row>
+      
     </div>
   );
 }
