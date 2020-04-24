5cea5ebdd56893bcea4ea818af4121fba1803e44
Petro Fedorenko <petewaitforletters@gmail.com>
2020-04-24 13:41:14 +0300

    feat: add categories and GET method

---
+++ src/JokesForm/JokesForm.constants.js
@@ -0,0 +1,28 @@
+export const apiUrl = 'https://api.chucknorris.io/jokes/';
+
+export const categoryData = [
+{ 
+  id: 'categoryAnimal',
+  type: 'radio',
+  name: 'category',
+  value: 'animal'
+ },
+ { 
+  id: 'categoryCareer',
+  type: 'radio',
+  name: 'category',
+  value: 'career'
+ },
+ { 
+  id: 'categoryCelebrity',
+  type: 'radio',
+  name: 'category',
+  value: 'celebrity'
+ },
+ { 
+  id: 'categoryDev',
+  type: 'radio',
+  name: 'category',
+  value: 'dev'
+ }
+];
--- src/JokesForm/JokesForm.js
+++ src/JokesForm/JokesForm.js
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import { Form, Button } from "react-bootstrap";
+import axios from "axios";
 import "./JokesForm.scss";
+import { apiUrl, categoryData } from "./JokesForm.constants";
 
 function JokesForm() {
   const [state, setState] = useState({
@@ -8,12 +10,25 @@ function JokesForm() {
     value: "",
   });
 
+  async function getJoke(params) {
+    try {
+      const response = await axios.get(`${apiUrl}${params}`);
+      console.log(response);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
   function handleChangeRadioButtons(event) {
-    if (state.jokeType === "Search") {
+    if (state.jokeType !== "Random") {
       setState({ jokeType: event.target.value, value: "" });
     } else {
       setState({ ...state, jokeType: event.target.value });
     }
+  }
+
+  function handleChangeCategory(event) {
+    setState({ ...state, value: event.target.value });
   }
 
   function handleChangeSearchField(event) {
@@ -26,13 +41,18 @@ function JokesForm() {
       params = "random";
     } else if (state.jokeType === "Category") {
       params = "random?category=";
+      if (!state.value) {
+        return alert("You need to choose the category of joke");
+      }
     } else if (state.jokeType === "Search") {
       params = "search?query=";
+      if (!state.value) {
+        return alert("You need to write what to search");
+      }
     } else {
-      alert("You need to choose the type of joke");
-      return;
+      return alert("You need to choose the type of joke");
     }
-    alert(params + state.value);
+    getJoke(params + state.value);
     setState({ jokeType: "", value: "" });
     event.preventDefault();
   }
@@ -62,6 +82,21 @@ function JokesForm() {
           checked={state.jokeType === "Category"}
           onChange={handleChangeRadioButtons}
         />
+        {state.jokeType === "Category" ? (
+          categoryData.map(data => (
+            <Form.Check
+          custom
+          className={data.name}
+          type={data.type}
+          name={data.name}
+          id={data.id}
+          label={data.value}
+          value={data.value}
+          checked={state.value === data.value}
+          onChange={handleChangeCategory}
+        />
+          ))
+        ) : null}
         <Form.Check
           custom
           type="radio"
