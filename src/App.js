9538fa19dd4b3402905ee254760755d710e55eee
Petro Fedorenko <petewaitforletters@gmail.com>
2020-04-25 13:03:12 +0300

    feat: implement favourite button and component

--- src/App.js
+++ src/App.js
@@ -3,31 +3,61 @@ import { Row, Col } from "react-bootstrap";
 import "./App.scss";
 import JokesForm from "./JokesForm";
 import JokesList from "./JokesList";
+import Favourite from "./Favourite";
 
 function App() {
-  const [state, setState] = useState({});
+  const [state, setState] = useState({
+    jokeData: {},
+    isOpenFavourite: false,
+    favouriteDate: {
+      data: {
+        result: [],
+      },
+    },
+  });
 
   function getData(data) {
-    setState(data);
+    setState({ ...state, jokeData: data });
+  }
+
+  function handleFavourite() {
+    setState({ ...state, isOpenFavourite: !state.isOpenFavourite });
   }
 
   return (
-    <div className="App">
+    <div className={state.isOpenFavourite ? "App App--grey" : "App"}>
       <header className="header">
-        <div className="header__headline">MSI 2020</div>
-        <div className="header__favourite">Favourite</div>
+        <div className="header__headline">
+          {state.isOpenFavourite ? "" : "MSI 2020"}
+        </div>
+        <button
+          className={
+            state.isOpenFavourite
+              ? "header__favourite header__favourite--open"
+              : "header__favourite"
+          }
+          onClick={handleFavourite}
+        >
+          Favourite
+        </button>
       </header>
-      <main>
-        <Row>
-          <Col className="column" xl={8}>
-            <JokesForm getData={getData} />
-            <JokesList joke={state} />
-          </Col>
-          <Col className="column" xl={4}>
-            Favourite (in desktop)
-          </Col>
-        </Row>
-      </main>
+      {state.isOpenFavourite ? (
+        <Favourite />
+      ) : (
+        <>
+          <main>
+            <Row>
+              <Col className="column" xl={8}>
+                <JokesForm getData={getData} />
+                <JokesList joke={state.jokeData} />
+              </Col>
+              <Col className="column" xl={4}>
+                Favourite (in desktop)
+              </Col>
+            </Row>
+          </main>
+        </>
+      )}
     </div>
   );
 }
---
+++ src/Favourite/Favourite.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+function Favourite() {
+  //<JokesList joke={data} />
+  return <div className="favourite">Favourite</div>;
+}
+
+export default Favourite;
