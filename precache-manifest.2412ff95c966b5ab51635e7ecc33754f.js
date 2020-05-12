d066029801fa3d99ef4cd37d43d63b6d96497a9d
Petro Fedorenko <petewaitforletters@gmail.com>
2020-05-12 16:07:13 +0300

    Updates

---
+++ precache-manifest.2412ff95c966b5ab51635e7ecc33754f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "358a5457b8e63421a1e39f3343dcaa8a",
+    "url": "/chuck-norris-app/index.html"
+  },
+  {
+    "revision": "ecc64314f6f0ea6e36c4",
+    "url": "/chuck-norris-app/static/css/main.c7598f1a.chunk.css"
+  },
+  {
+    "revision": "fff201d4165249dd75b7",
+    "url": "/chuck-norris-app/static/js/2.c4c6670d.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/chuck-norris-app/static/js/2.c4c6670d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ecc64314f6f0ea6e36c4",
+    "url": "/chuck-norris-app/static/js/main.742a602d.chunk.js"
+  },
+  {
+    "revision": "cf8296f4f2fcbaa393b9",
+    "url": "/chuck-norris-app/static/js/runtime-main.75b3226e.js"
+  }
+]);
